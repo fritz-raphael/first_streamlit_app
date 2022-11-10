1236dff73c9ed405703db3f2987b97be12c31167
Raphael Fritz <raphael.fritz@gmail.com>
2022-11-10 07:57:19 +0100

    Add Try/Except block for fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,18 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write(f'The user entered {fruit_choice}.')
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Take the json version of the response and normalize it 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# output on the screen as a table 
-streamlit.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice: 
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        # Take the json version of the response and normalize it 
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        # output on the screen as a table 
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 # don't run anything past here until troubleshooting is done 
 streamlit.stop()
