a7a55d8510da5319f35d6a8f8d748b9d4027bde1
Raphael Fritz <raphael.fritz@gmail.com>
2022-11-10 08:09:04 +0100

    Add columns to fruit table

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ def get_fruit_load_list():
 if streamlit.button("Get fruit load list"): 
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
+    streamlit.dataframe(my_data_rows, columns=["Number", "Fruit"])
 
 streamlit.stop()
 # Allow the user to add a fruit to the list 
